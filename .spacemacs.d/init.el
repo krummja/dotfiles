da1cd483467247e76e8e58de84f6d0ea0aa5462a
krummja <crumja4@gmailcom>
2020-06-24 18:49:02 -0400

    backing up before merge

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -1,13 +1,17 @@
 ;; -*- mode: emacs-lisp -*-
 
+;; S A K U R A Spacemacs
+
 (defun dotspacemacs/layers ()
   (setq-default
    dotspacemacs-distribution 'spacemacs
-   dotspacemacs-enable-lazy-installation 'unused
-   dotspacemacs-ask-for-lazy-installation t
-   dotspacemacs-configuration-layer-path '()
+   dotspacemacs-configuration-layer-path '("~/.spacemacs.d/layers/")
    dotspacemacs-configuration-layers
-   '((auto-completion :variables
+   '((config   :location local)
+     (display  :location local)
+     (sakura   :location local)
+
+     (auto-completion :variables
                       auto-completion-enable-snippets-in-popup t
                       auto-completion-enable-sort-by-usage t)
      bibtex
@@ -44,15 +48,13 @@
    dotspacemacs-check-for-update nil
    dotspacemacs-elpa-subdirectory nil
    dotspacemacs-editing-style 'hybrid
-   dotspacemacs-verbose-loading nil
    dotspacemacs-startup-banner 'official
-   dotspacemacs-startup-lists '((recents . 5)
-                                (projects . 7))
+   dotspacemacs-startup-lists '((recents . 10)
+                                (projects . 5))
    dotspacemacs-startup-buffer-responsive t
    dotspacemacs-scratch-mode 'org-mode
    dotspacemacs-themes '(spacemacs-dark
                          spacemacs-light)
-   dotspacemacs-colorize-cursor-according-to-state t
    dotspacemacs-default-font '("Fira Code"
                                :size 14
                                :weight normal
@@ -65,46 +67,9 @@
    dotspacemacs-emacs-leader-key "M-m"
    dotspacemacs-major-mode-leader-key ","
    dotspacemacs-major-mode-emacs-leader-key "C-M-m"
-   dotspacemacs-distinguish-gui-tab nil
-   dotspacemacs-remap-Y-to-y$ nil
-   dotspacemacs-retain-visual-state-on-shift t
-   dotspacemacs-visual-line-move-text nil
-   dotspacemacs-ex-substitute-global nil
-   dotspacemacs-default-layout-name "Default"
-   dotspacemacs-display-default-layout nil
-   dotspacemacs-auto-resume-layouts nil
-   dotspacemacs-large-file-size 1
-   dotspacemacs-auto-save-file-location 'cache
-   dotspacemacs-max-rollback-slots 5
-   dotspacemacs-helm-resize nil
-   dotspacemacs-helm-no-header nil
-   dotspacemacs-helm-position 'bottom
-   dotspacemacs-helm-use-fuzzy 'always
-   dotspacemacs-enable-paste-transient-state nil
-   dotspacemacs-which-key-delay 0.4
-   dotspacemacs-which-key-position 'bottom
-   dotspacemacs-loading-progress-bar t
-   dotspacemacs-fullscreen-at-startup nil
-   dotspacemacs-fullscreen-use-non-native nil
-   dotspacemacs-maximized-at-startup nil
-   dotspacemacs-active-transparency 90
-   dotspacemacs-inactive-transparency 90
-   dotspacemacs-show-transient-state-title t
-   dotspacemacs-show-transient-state-color-guide t
-   dotspacemacs-mode-line-unicode-symbols t
    dotspacemacs-mode-line-theme 'doom
-   dotspacemacs-smooth-scrolling t
-   dotspacemacs-line-numbers nil
-   dotspacemacs-folding-method 'evil
-   dotspacemacs-smartparens-strict-mode nil
-   dotspacemacs-smart-closing-parenthesis nil
-   dotspacemacs-highlight-delimiters 'all
-   dotspacemacs-persistent-server nil
-   dotspacemacs-search-tools '("ag" "pt" "ack" "grep")
-   dotspacemacs-default-package-repository nil
    dotspacemacs-whitespace-cleanup 'trailing
    ))
-
 
 (defun my-save-if-bufferfilename()
   (if (buffer-file-name)
@@ -141,10 +106,8 @@
   (require 'org-projectile)
   (require 'org-roam)
 
-
   ;; Keybinding Modifications
-  (global-unset-key (kbd "C-z"))
-
+  ;; (global-unset-key (kbd "C-z"))
 
   ;; User Variables
   ;;;; Paths
@@ -199,7 +162,7 @@
   ;;;; LaTeX
   (setq TeX-auto-save t)
   (setq TeX-parse-self t)
-  (setq org-latex-create-formula-image-program 'dvipng)
+  (setq org-preview-latex-default-process 'dvipng)
   (setq org-format-latex-options (plist-put org-format-latex-options :scale 1.5))
   (setq-default TeX-master nil)
 
